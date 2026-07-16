3d3a21e07860920820fdc605c85a63bceaecc289
Trusten Lehmann-Karp <tlehmannkarp@g.hmc.edu>
2026-07-16 08:57:24 -0400

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map1={}
+        for n in strs:
+            key=[0]*26
+            for letter in list(n):
+                key[ord(letter)-ord('a')]+=1
+            key=tuple(key)
+            if key in map1:
+                map1[key].append(n)
+            else:
+                map1[key] = [n]
+
+        result = []
+        for k, v in map1.items():
+            result.append(v)
+        
+        return result
