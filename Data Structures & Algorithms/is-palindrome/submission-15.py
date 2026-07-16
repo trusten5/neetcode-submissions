324dea6e1bb6f96cd45768a0e9f5a6d47ee25796
Trusten Lehmann-Karp <tlehmannkarp@g.hmc.edu>
2026-07-16 13:31:12 -0400

    Add: is-palindrome - submission-15

---
+++ Data Structures & Algorithms/is-palindrome/submission-15.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s)-1
+        s= s.lower()
+
+        while i<j:
+            if i<j and not s[j].isalnum():
+                j-=1
+            elif i<j and not s[i].isalnum():
+                i+=1
+            elif s[i] != s[j]:
+                return False
+            else:
+                i+=1
+                j-=1
+            
+            
+        
+        return True
