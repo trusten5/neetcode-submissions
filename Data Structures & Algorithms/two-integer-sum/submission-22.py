68d48b43a3b7f7119ff37c3a1af92f9b003d02db
Trusten Lehmann-Karp <tlehmannkarp@g.hmc.edu>
2026-07-20 09:11:17 -0400

    Add: two-integer-sum - submission-22

---
+++ Data Structures & Algorithms/two-integer-sum/submission-22.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map1={}
+
+        for n in range(len(nums)):
+            if nums[n] in map1:
+                return [map1[nums[n]],n]
+            else:
+                map1[target-nums[n]]=n
