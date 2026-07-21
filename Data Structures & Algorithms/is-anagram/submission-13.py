c1ead488176956b81587b9ff9b76fc76886ab557
Trusten Lehmann-Karp <tlehmannkarp@g.hmc.edu>
2026-07-21 08:45:45 -0400

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        map1={}
+        map2={}
+        
+        for l in s:
+            if l in map1:
+                map1[l]+=1
+            else:
+                map1[l]=1
+
+        for l in t:
+            if l in map2:
+                map2[l]+=1
+            else:
+                map2[l]=1
+
+        return map1==map2
