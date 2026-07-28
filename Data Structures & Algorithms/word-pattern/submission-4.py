7f4b2f51ebd3dad73377ca67a23baea3c76aee44
Trusten Lehmann-Karp <tlehmannkarp@g.hmc.edu>
2026-07-28 12:10:05 -0400

    Add: word-pattern - submission-4

---
+++ Data Structures & Algorithms/word-pattern/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        seen={}
+        store = set()
+        if len(pattern) != len(words):
+            return False
+        else:
+            for l in range(len(pattern)):
+                print(pattern[l], words[l])
+                if pattern[l] in seen:
+                    if seen[pattern[l]] != words[l]:
+                        return False
+                else:
+                    if words[l] in store:
+                        return False
+                    seen[pattern[l]]=words[l]
+                    store.add(words[l])
+        
+        
+        return True
