093ca8e7aab2db660e28680f5d11633158ac50d9
Trusten Lehmann-Karp <tlehmannkarp@g.hmc.edu>
2026-08-04 10:54:09 -0400

    Add: validate-parentheses - submission-36

---
+++ Data Structures & Algorithms/validate-parentheses/submission-36.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        change = {'}':'{', ']':'[',')':'('}
+        stack = []
+
+        for l in s:
+            if l in change:
+                if stack and stack[-1]==change[l]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(l)
+        
+        return not stack
