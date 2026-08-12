2f4cd6ec4d1eb90b261c84bbc0e4977e4adf488b
Trusten Lehmann-Karp <tlehmannkarp@g.hmc.edu>
2026-08-12 14:55:16 -0400

    Add: count-the-number-of-consistent-strings - submission-1

---
+++ Data Structures & Algorithms/count-the-number-of-consistent-strings/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        count=len(words)
+        for w in words:
+            print(w)
+            for l in w:
+                print(l, l not in allowed)
+                if l not in allowed:
+                    count-=1
+                    break
+                print(count)
+
+        return count
